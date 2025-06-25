85fabef37df3d2ac5ddc5f5dbd8d1455042e2c7d
Naren Mudivarthy <narenuday595@gmail.com>
2025-06-24 17:28:37 -0700

    some changes

--- ram.py
+++ ram.py
@@ -1,20 +1,47 @@
 from fuzzysearch import find_near_matches
 from ramayanam import Ramayanam
 from pyfiglet import Figlet
-from prompt_toolkit import prompt
-from iterfzf import iterfzf
+from prompt_toolkit import prompt, print_formatted_text, HTML, ANSI
+
 
 f = Figlet(font='slant')
 print(f.renderText('Sri Ramayanam'))
-print()
+
 
 r = Ramayanam.load()
 
 s = r.kanda(1).sarga(8).sloka(20)
 
+
+
+
+
+### Sanskrit-parser
+def callParser():
+    import requests
+
+    url = "https://sanskrit-parser.appspot.com/sanskrit_parser/v1/splits/वाल्मीकिर्मुनिपुङ्गवम्"
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.json()
+
+print_formatted_text(ANSI('\x1b[31mhello \x1b[32mworld'))
+
+# x = callParser()
+# print_formatted_text(HTML('<aaa fg="ansiwhite" bg="ansigreen">{}</aaa>'.format(x['splits'])))
+
+
+
 for k in r:
-    print(k)
     for s in k:
-        print(s)
         for sl in s:
-            print(sl)
+            trans = str(sl.translation).strip()
+            if trans:
+                print(trans, type(trans))
+                x = find_near_matches(trans, 'ram', max_l_dist=1)
+                if x:
+                    print(x)
